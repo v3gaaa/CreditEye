412f7e677be0c008fdd9a10ec8a80e73edd628b1
Spiegelin <diego15.espejo@gmail.com>
2024-09-14 17:04:02 -0600

    Add shadcn components

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,13 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/index.css";` // Importa tu archivo CSS
+      }
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
