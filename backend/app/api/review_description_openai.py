5034514ebb0fed47a365ead6a4ea73cc0f7279d0
Pablo Heredia <pheredia2004@gmail.com>
2024-09-15 10:39:30 -0600

    Estructured output y verificacion de si dar credito

--- backend/app/api/review_description_openai.py
+++ backend/app/api/review_description_openai.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 from typing import List, Optional
 from app.core.firestore_handler import save_request_data
 from app.models.get_info_model_openai import get_info_model_openai
-from app.core.openai_review import openai_review_final
+from app.core.openai_review import openai_review_final, openai_review_final_boolean, openai_review_relevant_data
 
 router = APIRouter()
 
@@ -12,11 +12,13 @@ async def get_review_desc(request_id: str):
     try:
         review_desc = get_info_model_openai(request_id)  # Función que obtenga la descripción del modelo
         final_inference = await openai_review_final(review_desc)
+        aproval = await openai_review_final_boolean(final_inference)
+        relevant_data = await openai_review_relevant_data(review_desc)
         if not review_desc:
             raise HTTPException(status_code=404, detail="Review description not found")
         
-        relevant_data = {"address": "San Pedro Garza", "phone": "1234567890", "email": "tec@gg.com"}
+        #relevant_data = {"address": "San Pedro Garza", "phone": "1234567890", "email": "tec@gg.com"}
         
-        return {"should_approve": True, "explanation": final_inference, "relevant_data": relevant_data}
+        return {"should_approve": bool(aproval), "explanation": final_inference, "relevant_data": relevant_data}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- backend/app/core/openai_review.py
+++ backend/app/core/openai_review.py
@@ -47,3 +47,42 @@ async def openai_review_final(documents_info):
     except Exception as e:
         print(f"Error during OpenAI request: {e}")
         return {}
+
+async def openai_review_final_boolean(final_inference):
+    try:
+        completion = client.chat.completions.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": "Eres un experto en asesorar PYMEs para ayudarles a obtener créditos. Tu objetivo es analizar documentos de onboarding de empresas y extraer los insights más importantes, evaluando el riesgo financiero y proporcionando recomendaciones sobre su viabilidad crediticia. Estás especializado en evaluar información clave como historial financiero, solvencia, garantías, y perfil de riesgo."},
+                {"role": "user", "content": f"Basado en esta decision final, responde con 'True' o 'False' si la empresa debería ser aprobada para un crédito. Aquí está la información:\n{final_inference}. SOLO RESPONDE CON 'True' o 'False'."}
+
+            ]
+        )
+
+        generated_text = completion.choices[0].message.content
+        return generated_text
+
+    except Exception as e:
+        print(f"Error during OpenAI request: {e}")
+        return {}
+    
+async def openai_review_relevant_data(documents_info):
+
+    relevant_data = {"address": "San Pedro Garza", "phone": "1234567890", "email": "tec@gg.com"}
+    try:
+        completion = client.chat.completions.create(
+            model="gpt-3.5-turbo-0125",
+            messages=[
+                {"role": "system", "content": "Eres un experto en asesorar PYMEs para ayudarles a obtener créditos. Tu objetivo es analizar documentos de onboarding de empresas y extraer los insights más importantes, evaluando el riesgo financiero y proporcionando recomendaciones sobre su viabilidad crediticia. Estás especializado en evaluar información clave como historial financiero, solvencia, garantías, y perfil de riesgo."},
+                {"role": "user", "content": f"En base a esta informacion recabada de los documentos financieros y personales de una pyme, necesito que me des alguna data muy relevante que encuentres de los documentos. Aquí está la información:\n{documents_info}. Responde en formato de json, como este, dependiendo de que tanto o poco encuentres: {relevant_data}"}
+
+            ],
+            response_format={"type": "json_object"}
+        )
+
+        generated_text = completion.choices[0].message.content
+        return generated_text
+
+    except Exception as e:
+        print(f"Error during OpenAI request: {e}")
+        return {}
