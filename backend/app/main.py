dfe2be6c727a9616d7ee69c54b3c7d1ad99693a8
Sebastian Vega <svsm03@hotmail.com>
2024-09-15 06:15:38 -0600

    Conexion backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-#from app.api import upload, evaluation, vision, health, send_info
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import health, send_info, basic_request, review_info, get_document, edit_risk_score, approve_reject_request, get_all_requests, vision, update_review_info
 import uvicorn
 from dotenv import load_dotenv
@@ -9,10 +9,17 @@ load_dotenv()
 
 app = FastAPI()
 
-# Incluir las rutas de los diferentes módulos
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Update this with your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+# Include routes from different modules
 app.include_router(health.router)
-#app.include_router(upload.router)
-#app.include_router(evaluation.router)
 app.include_router(vision.router)
 app.include_router(send_info.router)
 app.include_router(basic_request.router)
@@ -22,7 +29,6 @@ app.include_router(edit_risk_score.router)
 app.include_router(approve_reject_request.router)
 app.include_router(get_all_requests.router)
 app.include_router(update_review_info.router)
-
 
 if __name__ == "__main__":
     port = os.getenv("PORT", 5555)
